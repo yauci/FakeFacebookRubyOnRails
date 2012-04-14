aeaf38cba7d2b495762e2d60022d589e8d76a2ac
yauci <alu3338@gmail.com>
2012-04-14 13:59:37 +0100

    el primer commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FakeFacebook::Application.config.secret_token = 'c6f4bfa81836787559551730e366d0940eef214f930c4d221c0717585410dce44a584c4f6bc2766fc5bddf8d69067bca6feee197ee4cee7a6a34f062cf777589'
